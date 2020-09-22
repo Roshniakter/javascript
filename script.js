89bbe389046346c65f4359c00f79031abe7feb07
Roshniakter <71695341+Roshniakter@users.noreply.github.com>
2020-09-22 17:37:17 +0800

    welcome

---
+++ script.js
@@ -0,0 +1,199 @@
+alert("ROshni akter  :visit To My Site ")
+
+var a, b, c;
+a = 30;
+b = 20;
+a = 40;
+c = a + b;
+document.write(c);
+document.write("<br>");
+
+//first
+let x, y, z;
+x = 250;
+y = 10;
+z = x * y;
+document.write(z);
+document.write("<br>");
+
+//second
+const A = 50;
+const B = 45;
+const C = A + B;
+document.write(C);
+document.write("<br>");
+
+//third
+var X = "Welcome";
+var Y = 2544;
+var Z = true;
+var k = ["html", "css", "javacsript"];
+var f = {
+    firstname: "Roshni",
+};
+document.write(X);
+document.write("<br>");
+document.write(typeof X);
+document.write("<br>");
+document.write(typeof Y);
+document.write("<br>");
+document.write(typeof Z);
+document.write("<br>");
+document.write(typeof f);
+document.write("<br>");
+document.write(typeof k);
+document.write("<br>");
+
+//fourth
+var h = 10;
+var i = 7;
+h **= i;
+document.write(f);
+document.write("<br>");
+
+//fiveth
+console.log(10 > 50);
+console.log(10 == 50);
+console.log(10 < 50);
+var D, E;
+D = 250;
+E = 3688;
+console.log(D <= E);
+
+var F, G;
+F = 15;
+G = 250;
+if (F < G) {
+    document.write(G + "..is greater than 15");
+    document.write("<br>");
+}
+
+var F = 30;
+if (F > 30) {
+    document.write("true")
+    document.write("<br>");
+} else {
+    document.write("false")
+    document.write("<br>");
+}
+
+//result
+var num = 99;
+if (num >= 80 && num <= 100) {
+    document.write("A+")
+    document.write("<br>")
+} else if (num < 80 && num >= 70) {
+    document.write("A")
+    document.write("<br>")
+} else if (num < 70 && num >= 60) {
+    document.write("A-")
+    document.write("<br>")
+} else if (num < 60 && num >= 50) {
+    document.write("B")
+    document.write("<br>")
+} else if (num < 50 && num >= 33) {
+    document.write("pass")
+    document.write("<br>")
+} else if (num < 33) {
+    document.write("Fail")
+    document.write("<br>")
+} else {
+    document.write("wrong")
+    document.write("<br>")
+}
+
+//days
+var day = 1;
+switch (day) {
+    case 1:
+        document.write("today is friday")
+        document.write("<br>")
+
+        break;
+    case 2:
+        document.write("today is Saturday")
+        document.write("<br>")
+        break;
+    case 3:
+        document.write("today is sunday")
+        document.write("<br>")
+        break;
+    case 4:
+        document.write("today is monday")
+        document.write("<br>")
+        break;
+    case 5:
+        document.write("today is tuesday")
+        document.write("<br>")
+        break;
+    case 6:
+        document.write("today is wednesday")
+        document.write("<br>")
+        break;
+    case 7:
+        document.write("today is thursday")
+        document.write("<br>")
+        break;
+    default:
+        document.write("wrong")
+        document.write("<br>")
+        break;
+}
+
+var age = 28;
+switch (true) {
+    case (age > 10 && age <= 15):
+        document.write("you are small")
+        document.write("<br>")
+        break;
+    case (age > 16 && age <= 22):
+        document.write("You are Young")
+        document.write("<br>")
+        break;
+    case (age > 22):
+        document.write("You are perfect")
+        document.write("<br>")
+        break;
+    default:
+        document.write("You are not eligible")
+        document.write("<br>")
+        break;
+}
+
+var h=10;
+var i=20;
+if (h<i) {
+    alert("welcome to our side")
+    document.write("<br>")
+}else{
+    alert("we are great")
+    document.write("<br>")
+}
+
+//roshni
+ function hello(){
+    document.write("good work");
+    document.write("<br>");
+}
+hello();
+hello();
+hello();
+hello();
+hello();
+hello();
+hello();
+hello();
+hello();
+
+var a=1;
+while (a<=5) {
+    document.write( a + "we are fine <br>");
+    a=a+1;
+}
+var a=1;
+while (a<=2) {
+    document.write( a + "thanks for visit <br>");
+    a=a+2;
+}
+
+
